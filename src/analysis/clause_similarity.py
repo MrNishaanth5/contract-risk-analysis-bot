1cb6399c59dc3742aec4b6cd073dcb6ac51acc07
Nishaanth <147023689+MrNishaanth5@users.noreply.github.com>
2026-02-06 23:13:51 +0530

    Update clause_similarity.py

--- src/analysis/clause_similarity.py
+++ src/analysis/clause_similarity.py
@@ -1,9 +1,11 @@
 import spacy
+import en_core_web_sm
 
-nlp = spacy.load("en_core_web_sm")
+nlp = en_core_web_sm.load()
 
 def compute_similarity(clause_text, template_text):
     doc1 = nlp(clause_text)
     doc2 = nlp(template_text)
 
     return doc1.similarity(doc2)
+
