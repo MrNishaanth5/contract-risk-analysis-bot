7f57698fdddc6ed780dafcfb9da01ab997e0ecf6
Nishaanth <147023689+MrNishaanth5@users.noreply.github.com>
2026-02-06 23:24:44 +0530

    Update clause_similarity.py

--- src/analysis/clause_similarity.py
+++ src/analysis/clause_similarity.py
@@ -1,11 +1,17 @@
 import spacy
-import en_core_web_sm
+from spacy.util import is_package
+import subprocess
+import sys
 
-nlp = en_core_web_sm.load()
+if not is_package("en_core_web_sm"):
+    subprocess.run([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
+
+nlp = spacy.load("en_core_web_sm")
 
 def compute_similarity(clause_text, template_text):
     doc1 = nlp(clause_text)
     doc2 = nlp(template_text)
 
     return doc1.similarity(doc2)
+
 
